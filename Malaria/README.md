ac530fce908601749d0c6b1e2f8040b23c3ba25d
Emma Bolech <inf-25-2025@bioinf-serv2.cob.lu.se>
2026-03-05 00:33:01 +0100

    update readme and host scaffold script

--- Malaria/README.md
+++ Malaria/README.md
@@ -25,9 +25,25 @@ cp -r /home2/resources/binp29/Data/malaria .
 ```bash
 #!/usr/bin/bash
 
-gmes_petap.pl --ES --cores 10 --sequence resources/plasmodium_genomes/Plasmodium_vivax.genome --work_dir gene_prediction_vivax/
+gmes_petap.pl --ES --cores 10 --sequence resources/plasmodium_genomes/Plasmodium_vivax.genome --work_dir gene_prediction/gene_prediction_vivax/
 
 # move to the /tmp/PutGenomesHere
+
+#####################################################
+
+# inspecting other gene predictions / genomes and filling out info table ex.
+
+# number of genes
+cut -f3 knowlesi.gtf | grep "gene" | wc -l
+# 4953
+
+# size of genome
+grep -v '^>' Plasmodium_cynomolgi.genome | tr -d '\n' | wc -c
+# 26181343
+
+# counting genomic GC ex.
+grep -v '^>' Plasmodium_cynomolgi.genome | tr -d '\n' | awk '{s=$0; gc=gsub(/[GgCc]/,"",s); printf "%.6f\n", gc/length($0)*100}'
+# 39.078305
 
 ```
 
@@ -38,14 +54,14 @@ gmes_petap.pl --ES --cores 10 --sequence resources/plasmodium_genomes/Plasmodium
 ```bash
 #!/usr/bin/bash
 
-python3 removeScaffold.py Haemoproteus_tartakovskyi.raw.genome 30 Ht_genome_filered 3000
+python3 removeScaffold.py Haemoproteus_tartakovskyi.raw.genome 30 Ht_genome_filtered.genome 3000
 
 # original contig #
 grep "^>" Haemoproteus_tartakovskyi.raw.genome | wc -l
 # 15048
 
 # filtered contig #
-grep "^>" Ht_genome_filered | wc -l
+grep "^>" Ht_genome_filtered.genome | wc -l
 # 2222
 
 # 12826 excluded
@@ -55,25 +71,24 @@ grep "^>" Ht_genome_filered | wc -l
 ## 4. Gene Prediction for filtered H. tartakovsky using GeneMark.
 > Using a minimum of 10,000 contigs for training as most genes are not smaller than this. 
 
-
 ```bash
 #!/usr/bin/bash
 
 # total contigs
-grep "^>" Ht_genome_filered | wc -l
+grep "^>" Ht_genome_filtered.genome | wc -l
 # 2222
 
 # how many below 10,000
-grep ">" Ht_genome_filered | grep -o "Length=[0-9]*" | cut -d'=' -f2 | awk '$1 < 10000' | wc -l
+grep ">" Ht_genome_filtered.genome | grep -o "Length=[0-9]*" | cut -d'=' -f2 | awk '$1 < 10000' | wc -l
 # 1771
 
-grep ">" Ht_genome_filered | grep -o "Length=[0-9]*" | cut -d'=' -f2 | awk 'BEGIN{min=999999999; max=0} {if($1>max) max=$1;
+grep ">" Ht_genome_filtered.genome | grep -o "Length=[0-9]*" | cut -d'=' -f2 | awk 'BEGIN{min=999999999; max=0} {if($1>max) max=$1;
  if($1<min) min=$1} END{print "Min:", min, "\nMax:", max}'
 # Min: 3003 
 # Max: 64494
 
 # do the gene prediction 
-gmes_petap.pl --ES --min_contig 10000 --cores 10 --sequence resources/Ht_genome_filered --work_dir gene_prediction_tart/
+gmes_petap.pl --ES --min_contig 10000 --cores 10 --sequence resources/plasmodium_genomes/Ht_genome_filtered.genome --work_dir gene_prediction/gene_prediction_tart/
 
 ```
 
@@ -86,7 +101,7 @@ gmes_petap.pl --ES --min_contig 10000 --cores 10 --sequence resources/Ht_genome_
 cat ../Ht.gtf | sed "s/ GC=.*\tGeneMark.hmm/\tGeneMark.hmm/" > Ht2.gtf
 
 # use the parser to make a fasta file (-c and -p options used to output an amino acid fasta as well)
-gffParse.pl -i ../../resources/Ht_genome_filered -g ../Ht.gtf -b Ht_fasta -c -p
+gffParse.pl -i ../../resources/plasmodium_genomes/Ht_genome_filered -g ../Ht.gtf -b Ht_fasta -c -p
 
 # INFO: The gff or gtf file ../Ht2.gtf has successfully been parsed.
 #       There were 2120 scaffolds containing genes.
@@ -108,10 +123,10 @@ echo $BLASTDB
 # /home2/resources/blastdb
 
 # running blastx on the generated fna file
-nohup blastx -db SwissProt -query ../gene_prediction_tart/fasta/Ht_fasta.fna -out Ht.blastx -num_threads 10
+nohup blastx -db SwissProt -query ../gene_prediction/gene_prediction_tart/fasta/Ht_fasta.fna -out Ht.blastx -num_threads 10
 
 # running blastp on the generated faa file 
-nohup blastp -db SwissProt -query ../gene_prediction_tart/fasta/Ht_fasta.faa -out Ht.blastp -num_threads 10
+nohup blastp -db SwissProt -query ../gene_prediction/gene_prediction_tart/fasta/Ht_fasta.faa -out Ht.blastp -num_threads 10
 
 # confirming all queries were run
 grep "Query=" Ht.blastp | wc -l
@@ -119,7 +134,116 @@ grep "Query=" Ht.blastp | wc -l
 grep "Query=" Ht.blastx | wc -l
 # 3683
 
+######################################################
 
+# soft linking uniprot tax file 
+ln -s /resources/binp29/Data/malaria/taxonomy.dat taxonomy.dat
+ln -s /resources/binp29/Data/malaria/uniprot_sprot.dat uniprot_sprot.dat
 
+# running parser script which prints out host scaffolds (from the blastx output and within the fna fasta file)
+python3 scripts/datParser.py host_scaffold_cleaning/blast_ht/Ht.blastx gene_prediction/gene_prediction_tart/fasta/
+Ht_fasta.fna host_scaffold_cleaning/blast_ht/taxonomy.dat host_scaffold_cleaning/blast_ht/uniprot_sprot.dat > host_scaffold_cleaning/blastx_scaffolds.txt
+
+# running parser script which prints out host scaffolds (from the blastp output and within the faa fasta file)
+python3 scripts/datParser.py host_scaffold_cleaning/blast_ht/Ht.blastp gene_prediction/gene_prediction_tart/fasta/
+Ht_fasta.faa host_scaffold_cleaning/blast_ht/taxonomy.dat host_scaffold_cleaning/blast_ht/uniprot_sprot.dat > host_scaffold_cleaning/blastp_scaffolds.txt
+
+######################################################
+
+# comparing the host scaffolds from blastx output and blastp output
+wc -l blastp_scaffolds.txt 
+# 23 
+wc -l blastx_scaffolds.txt 
+# 32
+comm -12 blastx_scaffolds.txt blastp_scaffolds.txt | wc -l
+# 16 shared scaffolds between the two 
+
+# scaffolds only in blastp output
+comm -23 blastp_scaffolds.txt blastx_scaffolds.txt 
+# contig00021
+# contig00142
+# contig00338
+# contig00352
+# contig00356
+# contig00466
+# contig01393
+
+# scaffolds only in blastx output
+comm -13 blastp_scaffolds.txt blastx_scaffolds.txt 
+# contig00042
+# contig00044
+# contig00080
+# contig00232
+# contig00235
+# contig00238
+# contig00239
+# contig00621
+# contig00677
+# contig00970
+# contig01030
+# contig01132
+# contig01529
+# contig01562
+# contig01643
+# contig01899
+
+# because blastx will blast against many different reading frames i will take a strict filtering approach and filter based on all of the blastx host identified scaffolds and all of the remaining blastp host identified scaffolds
+
+######################################################
+
+# calling on the remove_host_scaffolds.py script to remove the id'ed conitgs
+python3 scripts/remove_host_scaffolds.py -f resources/plasmodium_genomes/Ht_genome_filtered.genome -i host_scaffold_cleaning/combined_scaffolds.txt -o resources/plasmodium_genomes/Ht_malaria_only.genome
+
+# confirm final contig #
+grep "^>" Ht_malaria_only.genome | wc -l
+# 2183
+
+```
+
+## 7. Re-doing gene prediction on host-filtered Ht genome. 
+
+```bash
+#!/usr/bin/bash
+
+nohup gmes_petap.pl --ES --min_contig 10000 --cores 10 --sequence ../../resources/plasmodium_genomes/Ht_malaria_only.ge
+nome --work_dir .
+
+```
+
+## 8. Creating .faa amino acid fasta files for each genome gtf file.
+
+```bash
+#!/usr/bin/bash
+
+# ht
+cat ht_nohost.gtf | sed "s/ GC=.*\tGeneMark.hmm/\tGeneMark.hmm/" > Ht_nohost2.gtf
+gffParse.pl -i ../../resources/plasmodium_genomes/Ht_malaria_only.genome -g Ht_nohost2.gtf -b fasta/Ht -c -p
+
+# repeated for all other species
+
+# copying the relevant .faa files to the orthos directory
+
+# should have
+ls
+# Ht.faa  Pb.faa  Pc.faa  Pf.faa  Pk.faa  Pv.faa  Py.faa  Tg.faa
+
+```
+
+## 9. Finding orthologs with proteinortho and BUSCO.
+
+```bash
+#!/usr/bin/bash
+
+# setting up a conda env for necessary packages
+conda create -n busco_env -c conda-forge -c bioconda python=3.10 busco -y
+conda install -c bioconda proteinortho
+
+# running protein ortho 
+nohup proteinortho6.pl -cpus=10 {Ht,Pb,Pc,Pf,Pk,Pv,Py,Tg}.faa > proteinortho.log 2>&1 &
+
+# for proteinortho you must remove stop *
+for f in {Ht,Pb,Pc,Pf,Pk,Pv,Py,Tg}.faa; do
+  sed -i -E '/^>/! s/[^XOUBZACDEFGHIKLMNPQRSTVWYxoubzacdefghiklmnpqrstvwy]//g; /^$/d' "$f"
+done
 
 ```
---
+++ Malaria/scripts/remove_host_scaffolds.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+remove_host_scaffolds.py
+
+Description: 
+
+User-defined functions:  
+    1. 
+
+Non-standard modules: 
+    Path: imported from pathlib to define working directory.
+    sys: imported to safely exit the program when necessary.
+    re: imported to use regex modules to check for matching characters with ex. re.findall.
+    math: imported to use natrual log equation.
+    pandas: imported to store data in DataFrames and output files using meaningful formatting
+    argparse: imported to define command line arguments using flags.
+    os: imported to split output file names by the extension.
+    
+Procedure:
+    1. 
+
+Input: 
+Output: 
+
+Usage: 
+python3 remove_host_scaffolds.py Ht_genome_filtered.genome combined_scaffolds.txt Ht_malaria_only.genome
+*** last argument is optional 
+*** -o if last argument is not given "_.genome" is used by default
+
+Version: 1.00
+Date: 2026-03-04
+Name: Emma Bolech
+"""
+
+#%%
+###############################################################################
+# Importing necessary non-standard modules.
+###############################################################################
+
+from pathlib import Path
+
+import sys
+
+import re
+
+import argparse
+
+import os
+
+#%%
+###############################################################################
+# Defining functions.
+###############################################################################
+
+'''
+host_id_scaffolds takes the input txt file and creates a list of the host identified scaffold names. 
+This list is used in the genome_filtering function to filter out the host identified scaffolds from the input fasta file.
+'''
+
+def host_id_scaffolds(textfile: str) -> list: 
+    with open(textfile, "r") as file:
+        scaffolds_list = list() #create an empty list to append the scaffold names to
+        for line in file:
+            line = line.strip() #strip whitespace
+            scaffolds_list.append(line) #append the scaffold names to the list
+
+    return scaffolds_list #return the list of scaffold names
+
+
+'''
+genome_filtering takes the input fasta file and the list of host identified scaffold names 
+and creates a dictionary of the contig names and sequences for the contigs that are not in the list 
+of host identified scaffolds.
+'''
+
+def genome_filtering(fastafile: str, scaffolds_list: list) -> dict:
+    with open(fastafile, "r") as file:
+        genome_dict = dict() #create an empty dictionary to store the contig names and sequences
+        for line in file:
+            line = line.strip() #strip whitespace
+            if line.startswith(">"): #if the line starts with a > it is a header line
+                name_match = re.search(r"^>?(contig\d+)\b", line)
+                if name_match:
+                    name = name_match.group(1) #save the contig name
+                    if name not in scaffolds_list: #if the contig name is not in the list of host identified scaffolds
+                        genome_dict[line] = "" #add the contig name as a key to the dictionary with an empty string as the value
+                        sequence = next(file).strip() #save the next line as the sequence
+                        genome_dict[line] = sequence #add the sequence as the value for the contig name
+                    else: #if the contig name is in the list of host identified scaffolds
+                        continue #skip the line and the next line (the sequence)
+            else: #if the line does not start with a > it is a sequence line
+                continue #skip the line
+
+        return genome_dict #return the dictionary of contig names and sequences
+    
+
+
+#%%
+############################################################################### 
+# Establish working directory with Path.
+###############################################################################
+
+#establish working directory and save into workingdirectory variable
+workingdirectory = Path.cwd()   
+print(f"This is your working directory: {workingdirectory}")    
+
+
+#%%
+###############################################################################
+# Setting up command line arguments and flags using argparse.
+###############################################################################
+
+'''
+argparse is used here to create a parser for this specific script. The second argument
+(after the script name) is defined to be the input_file and should be flagged with -f in 
+the command line. This argument is required. The help section is written to provide guidance to the user.
+The third argument is defined to be the second input_file and should be flagged with -i in the command line.
+This is required. The fourth argument is optional and is the output_file. By default it is _.genome. 
+
+'''
+
+parser = argparse.ArgumentParser(prog = "remove_host_scaffolds.py", description = "Removing host scaffolds from the input file.")
+parser.add_argument("-f","--input_file", required=True, help = "Please provide an input genome fasta file. This should be a .genome file with the same format as the Ht_genome_filtered.genome file provided in the repository. The contig names should match those found in provided combined_scaffolds.txt file.")
+parser.add_argument("-i", "--input_file02", required=True, help = "Please provide an input file with the same format as the combined_scaffolds.txt file provided in the repository. This file should contain the host identified contig names in a single column. The contig names should match those found in provided Ht_genome_filtered.genome file.")
+parser.add_argument("-o","--output_file", nargs="?", default="no_host.genome", help = "Specify an output file. Default is no_host.genome")
+
+args = parser.parse_args()  #setting the above variables as args
+
+'''
+Checks whether the input files exist in the working directory. Exits the program if not.
+'''
+
+input_path = Path(args.input_file)
+try:
+    if not input_path.exists():
+        raise Exception ("- Error: The provided input fasta file does not exist or is missing from the working directory.\nExiting...")
+except Exception as e:
+    print(e)
+    sys.exit()
+
+input_path = Path(args.input_file02)
+try:
+    if not input_path.exists():
+        raise Exception ("- Error: The provided input .txt file does not exist or is missing from the working directory.\nExiting...")
+except Exception as e:
+    print(e)
+    sys.exit()
+    
+
+#%%
+###############################################################################
+# Inspecting input files.
+###############################################################################
+
+#checking for empty input file
+with open(args.input_file, "r") as file:
+    total_lines = list()
+    for line in file:
+        total_lines.append(line) #append each line in a list
+    
+    length_input = len(total_lines) #save the number of total lines
+    try:
+        if length_input == 0: #if 0 assume empty and throw an error
+            raise Exception("- Error: The input file is empty.\nExiting...")
+    except Exception as e:
+        print(e)
+        sys.exit() #safely exit
+
+#checking that the first line is a header in the FASTA file
+with open(args.input_file) as file:
+    for line in file:
+        #if the first line starts with a > print the following check
+        if line.strip().startswith(">"):
+            print("- Check: The first line of the input file is a valid FASTA header line.")
+            #break out of the for loop if this is satisfied
+            break
+        else:
+            #if the first line does not start with an > print an error and exit the program safely
+            print("- Error: The first line of the input file is not a valid FASTA header line.\nExiting...")
+            sys.exit()
+
+###############################################################################
+
+#checking for 1 column in input txt file
+with open (args.input_file02, "r") as file:
+    
+    for line in file:
+        line_list = line.strip().split("\t")     #splits columns into a list by tabs
+        try:
+            if len(line_list) > 1: #if the length of columns is greater than 1 throw an error
+                raise Exception("- Error: The input file is more than one column. Please inspect.\nExiting...")
+        except Exception as e:
+            print(e)
+            sys.exit() #safely exit
+            
+    print("- Check: Input text file is correctly limited to 1 column.") #print in the end if passed
+        
+
+#%%
+###############################################################################
+# Filtering genome.
+###############################################################################
+
+'''
+Applying the functions to the input fasta file and outputing a final filtered genome file.
+'''
+
+host_names = host_id_scaffolds(args.input_file02) #save the list of host identified scaffold names from the input txt file
+
+
+genome = genome_filtering(args.input_file, host_names)
+
+with open(args.output_file, "w") as file:
+    for key, value in genome.items():
+        file.write(f"{key}\n{value}\n") #write the contig name and sequence to the output file with a newline in between    
+    
+
+###############################################################################
+
+# check whether output file contains any of the omitted contigs
+
+with open(args.output_file, "r") as file:
+    output_lines = list()
+    for line in file:
+        output_lines.append(line.strip()) #append each line in a list
+    
+    for scaffold in host_names:
+        if scaffold in output_lines:
+            print(f"- Error: The output file contains the following omitted contig: {scaffold}\nExiting...")
+            sys.exit() #safely exit
+
+
+
+#%%
+###############################################################################
+# Final message.
+###############################################################################
+
+#final print message that tells user what the input and output files are
+print(f"Your input files are: {args.input_file} and {args.input_file02}\nYour output file is: {args.output_file}.")
+
+print("Success!")
+
+
